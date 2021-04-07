3b5562d7bcc1c38e6dd6854f807c9a7275bd750b
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-04-07 10:18:11 -0700

    added global variables

--- script.js
+++ script.js
@@ -2,17 +2,25 @@
 var generateBtn = document.querySelector("#generate"); //querySelector returns the first element within the doc that matches selector
 var generatePassword = 0;
 var includeLowercase = "";
+var includeUppercase = "";
+var includeNumbers = "";
+var includeSpecialChar = "";
 var specialCharacters = ["!","/","#","$","%", "&", "/'", "(", ")", "*", "+", "/,", "-",
  ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", " \\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 
-console.log(specialCharacters);
+console.log(lowercaseLetters.length);
 
 console.log("This is what the query selector returns:");
 console.log(generateBtn);
 
-@@ -20,17 +27,34 @@ generateBtn.addEventListener("click", writePassword); //generateBtn is defined o
+generateBtn.addEventListener("click", writePassword); //generateBtn is defined o
 
 //generates password
 function generatePassword() {
